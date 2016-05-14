cdafb5c0efb23749c6bd4cbd3811e2d58aa02343
Michael Hopcroft <mhop@hopcroft.org>
2016-05-13 23:07:45 -0700

    Cleaned up code. Google style guidelines.

---
+++ sample/src/main/java/org/bitfunnel/test/DocumentFileProcessor.java
@@ -0,0 +1,184 @@
+package org.bitfunnel.test;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringReader;
+import java.util.Scanner;
+import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+
+
+public class DocumentFileProcessor {
+  Path input;
+  Path output;
+
+  Scanner scanner;
+  OutputStream outputStream;
+
+  String line;
+
+  static Pattern pattern =
+      Pattern.compile("<doc id=\"([^\"]*)\" url=\"[^\"]*\" title=\"([^\"]*)\">");
+
+  static Analyzer analyzer = new StandardAnalyzer();
+
+
+  public DocumentFileProcessor(Path input, Path output) {
+    this.input = input;
+    this.output = output;
+  }
+
+
+  public void ProcessFile() throws IOException, Exception {
+    System.out.println("Open file " + output + " inside of try.");
+    scanner = new Scanner(input);
+    ProcessAllDocuments();
+  }
+
+
+  private void ProcessAllDocuments() throws Exception {
+    try (FileScope scope = new FileScope()) {
+      while (scanner.hasNextLine()) {
+        ProcessOneDocument();
+      }
+    }
+  }
+
+
+  private void ProcessOneDocument() throws Exception {
+    try (DocumentScope scope = new DocumentScope()) {
+      ProcessDocumentHeader();
+      ProcessAllContentLines();
+      ProcessDocumentFooter();
+    }
+  }
+
+
+  private void ProcessDocumentHeader() throws Exception {
+    String line = GetLine();
+
+    Matcher matcher = pattern.matcher(line);
+    if (!matcher.find()) {
+      throw new RuntimeException("Malformed document header.");
+    }
+
+    String id = matcher.group(1);
+    String title = matcher.group(2);
+
+    try (StreamScope scope = new StreamScope("title")) {
+      emit(title);
+    }
+
+    // TODO: Should id be an integer in the file?
+    try (StreamScope scope = new StreamScope("id")) {
+      emit(id);
+    }
+  }
+
+
+  private void ProcessAllContentLines() throws Exception {
+    try (StreamScope scope = new StreamScope("content")) {
+      while (true) {
+        String line = PeekLine();
+        if (!IsDocumentEnd(line)) {
+          ProcessOneContentLine();
+        } else {
+          break;
+        }
+      }
+    }
+  }
+
+
+  private void ProcessOneContentLine() throws IOException {
+    String line = GetLine();
+
+    try (TokenStream tokenStream
+             = analyzer.tokenStream("contents", new StringReader(line))) {
+      tokenStream.reset();
+      CharTermAttribute term = tokenStream.addAttribute(CharTermAttribute.class);
+      while (tokenStream.incrementToken()) {
+        emit(term.toString());
+      }
+    }
+  }
+
+
+  private void emit(String text) {
+    System.out.println("\"" + text + "\\0\"");
+  }
+
+
+  private void ProcessDocumentFooter() {
+    String line = GetLine();
+
+    // TODO: REVIEW: Is this check really necessary?
+    if (!IsDocumentEnd(line)) {
+      throw new RuntimeException("Expected </doc>.");
+    }
+  }
+
+
+  private boolean IsDocumentEnd(String line) {
+    return line.startsWith("</doc>");
+  }
+
+
+  private String PeekLine() {
+    if (line == null) {
+      line = scanner.nextLine();
+    }
+    return line;
+  }
+
+
+  private String GetLine() {
+    PeekLine();             // Force line to be initialized.
+    String result = line;
+    line = null;
+    return result;
+  }
+
+
+  private class FileScope implements java.lang.AutoCloseable {
+    public FileScope() {
+    }
+
+    @Override
+    public void close() throws Exception {
+      // Write trailing '\0'
+      emit("");
+    }
+  }
+
+
+  private class DocumentScope implements java.lang.AutoCloseable {
+    public DocumentScope() {
+    }
+
+    @Override
+    public void close() throws Exception {
+      // Write trailing '\0'
+      emit("");
+    }
+  }
+
+
+  private class StreamScope implements java.lang.AutoCloseable {
+    public StreamScope(String name) {
+      emit(name);
+    }
+
+    @Override
+    public void close() throws Exception {
+      // Write trailing '\0'
+      emit("");
+    }
+  }
+}
